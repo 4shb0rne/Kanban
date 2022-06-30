fd9be7d90387e3b905afe08b89102e4329f5a5a4
Ash Borne <susannoizanagi6@gmail.com>
2022-06-30 14:46:04 +0700

    checklist progress

--- chello/src/views/components/ChecklistProgress.jsx
+++ chello/src/views/components/ChecklistProgress.jsx
@@ -1,18 +1,18 @@
-import { CheckedOutline } from "./Icons";
-
+import ProgressBar from "@ramonak/react-progress-bar";
 const ChecklistProgress = ({ todos }) => {
     const tasksCompleted = todos.filter((todo) => todo.done === true);
 
     return (
         <div className="flex sm:text-sm items-center justify-between text-xs">
-            <div className="bg-blue-100 flex rounded-3xl px-1 sm:px-1.5 py-0.5 text-blue-900 mr-1">
+            <div className="bg-blue-100 flex rounded-3xl px-1 sm:px-1.5 py-0.5 text-blue-900 mr-4">
                 <h4 className="tracking-wider">{`${tasksCompleted.length}/${todos.length}`}</h4>
             </div>
-            <progress
-                className="shadow-2xl w-12 sm:w-20"
-                value={tasksCompleted.length}
-                max={todos.length}
-            ></progress>
+            <ProgressBar
+                completed={(tasksCompleted.length / todos.length) * 100}
+                maxCompleted={100}
+                width="250px"
+                labelSize="50%"
+            ></ProgressBar>
         </div>
     );
 };
