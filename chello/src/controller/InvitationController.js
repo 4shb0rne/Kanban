b8c385b98c6bfc1f3863895a0d93032b74105800
ash <susannoizanagi6@gmail.com>
2022-07-18 15:48:53 +0700

    add code(3)

--- chello/src/controller/InvitationController.js
+++ chello/src/controller/InvitationController.js
@@ -1,13 +1,19 @@
-import { updateDoc } from "firebase/firestore";
+import { updateDoc, doc, deleteDoc } from "firebase/firestore";
 import firebase from "firebase/compat/app";
+import { db } from "../util/firebase-config";
+
 export const AcceptInvitation = async (NotificationID, WorkspaceID, UserID) => {
-  const ref = doc(db, "workspaces", WorkspaceID);
-  await updateDoc(ref, {
-    users: firebase.firestore.FieldValue.arrayUnion(UserID),
-  });
-  await deleteDoc(doc(db, `users/${UserID}/notifications`, NotificationID));
+    console.log("Test");
+    console.log(WorkspaceID);
+    const ref = doc(db, "workspaces", WorkspaceID);
+    await updateDoc(ref, {
+        users: firebase.firestore.FieldValue.arrayUnion(
+            doc(db, "users", UserID)
+        ),
+    });
+    await deleteDoc(doc(db, `users/${UserID}/notifications`, NotificationID));
 };
 
-export const DeclineInvitation = async (NotificationID) => {
-  await deleteDoc(doc(db, `users/${UserID}/notifications`, NotificationID));
+export const DeclineInvitation = async (NotificationID, UserID) => {
+    await deleteDoc(doc(db, `users/${UserID}/notifications`, NotificationID));
 };
