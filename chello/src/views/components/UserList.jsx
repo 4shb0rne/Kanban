68308ea99355b3594043a09d125956f9b3813da0
ash <susannoizanagi6@gmail.com>
2022-08-01 18:39:40 +0700

    minor change

---
+++ chello/src/views/components/UserList.jsx
@@ -0,0 +1,24 @@
+const UserList = () => {
+    <div class="flex justify-center">
+        <div class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+            <a
+                href="#!"
+                class="
+        block
+        px-6
+        py-2
+        border-gray-200
+        w-full
+        rounded-b-lg
+        focus:outline-none focus:ring-0
+        text-gray-400
+        cursor-default
+      "
+            >
+                A disabled link item
+            </a>
+        </div>
+    </div>;
+};
+
+export default UserList;
